af8ebe3a25a3334fd8b64c117c4d8690c8f135e7
levyhanunes <levyhanunes@gmail.com>
2021-04-22 22:17:16 -0400

    Arrumar Layout e vincular as views no controller 2

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    	
+    @IBOutlet weak var tfPWeight: UITextField!
+    
+    @IBOutlet weak var tfHeight: UITextField!
+    
+    @IBOutlet weak var lbResult: UILabel!
+    
+    @IBOutlet weak var ivResult: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
 
 }
 
